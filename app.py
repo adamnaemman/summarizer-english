e4104d6142bc1adfd70ff1d5639ac90f2007f6a8
adamnaemman <0damkim254@gmail.com>
2026-03-06 19:18:07 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from transformers import pipeline
+
+#1. Setup model (gune cache supaya tak load banyak kali)
+@st.cache_resource
+def load_summarizer():
+    return pipeline(
+        task="summarization",
+        model="facebook/bart-large-cnn"
+    )
+
+summarizer = load_summarizer()
+
+# 2. DEsign Interface(UI)
+st.title("Mentor Ai: Text Summarizer")
+st.subheader("Sila masukkan artikel yang panjang, meh aku buat ringkasan atau rumusan. Bahasa Inggeris je boleh tau.")
+
+#Kotak input
+input_text = st.text_area("Paste artikel kat sini (Min 100 patah perkataan): ", height=200)
+
+if st.button("Ringkaskan Sekarang!"):
+    if input_text:
+        with st.spinner('Tengah baca... jap eh...'):
+            #3. Proses summarization
+            # max_length = panjang, ringkasan, min_length = pendek mana kau nak
+            result = summarizer(input_text, max_length=130, min_length=30, do_sample=False)
+
+            summary = result[0]['summary_text']
+
+            st.success("Siap cipp!")
+            st.write("Hasil Ringkasan: ")
+            st.info(summary)
+    else:
+        st.warning("Eh, letaklah dulu baru boleh buat kerja, Ish Ish Ish")
